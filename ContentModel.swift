17c7ce0a124b6da29ead05f67890fd32e4745bab
Brady Robshaw <heybrady88@gmail.com>
2022-02-15 11:59:46 -0500

    -add get business function which interacts with the yelp API to retrieve business information from users location

--- ContentModel.swift
+++ ContentModel.swift
@@ -43,10 +43,46 @@ class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         // give us the location of the user
-        print(locations.first ?? "No Location")
+        let userLocation = locations.first
         
-        // stop requesting the location after we git it once
-        locationManager.stopUpdatingLocation()
+        if userLocation != nil {
+            // we have a location
+            // stop requesting the location after we git it once
+            locationManager.stopUpdatingLocation()
+            
+            // if we have the coordinates of the user send info to yelp API
+            getBusinesses(category: "restaurants", location: userLocation!)
+        }
         
+        
+        
+        // MARK: yelp API Methods
+        
+        func getBusinesses(category:String, location: CLLocation) {
+            // create url
+            let urlString = "https://api.yelp.com/v3/businesses/search?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&categories=\(category)&limit=6"
+            let url = URL(string: urlString)
+            
+            if let url = url {
+                // create url request
+                var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+                request.httpMethod = "GET"
+                request.addValue("Bearer 7GTO4GaNfVecl8JUVfHteNi-DtEuww4dfjrGnL4wr64yTWqilEYMeqpnjUqGgCvKbqIl9v-_CC0cIAYCNXQIJ1Hvcb-5jL6jAnlRqbcSkQZgP5R_tCOZRt3nOc0LYnYx", forHTTPHeaderField: "Authorization")
+                
+                // get URLSession
+                let session = URLSession.shared
+                
+                // create data task
+                let dataTask = session.dataTask(with: request) { (data, response, error) in
+                    // check if there isn't an error
+                    if error == nil {
+                        //print(response)
+                    }
+                }
+                
+                // start data task
+                dataTask.resume()
+            }
+        }
     }
 }
