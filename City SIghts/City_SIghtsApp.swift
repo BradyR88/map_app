caffc820d9a378f04588b13d585f09b9c9d15d31
Brady Robshaw <heybrady88@gmail.com>
2022-02-15 09:18:22 -0500

    Initial Commit

---
+++ City SIghts/City_SIghtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SIghtsApp.swift
+//  City SIghts
+//
+//  Created by Brady Robshaw on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct City_SIghtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City SIghts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City SIghts
+//
+//  Created by Brady Robshaw on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
